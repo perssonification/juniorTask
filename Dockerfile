778933f310e5d21761aaf518544427e226ccbc19
Perssonification <krispinpers@gmail.com>
2025-08-04 22:31:38 +0200

    Refactor Dockerfile and README, remove unused files and update index.php for improved structure and clarity

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,6 @@ RUN a2enmod rewrite
 
 RUN docker-php-ext-install pdo pdo_mysql
 
-RUN pecl install -o -f xdebug-3.4.5 \
-    && docker-php-ext-enable xdebug
-
-COPY php.ini /usr/local/etc/php/conf.d/
-
 COPY . /var/www/html/
 WORKDIR /var/www/html/
 
--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $categories = json_decode(file_get_contents("https://api.chucknorris.io/jokes/ca
 <head>
   <meta charset="UTF-8" />
   <title>Chuck Norris Joke Generator</title>
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="/View/style.css" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
